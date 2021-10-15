fb06fd3ec5d052ce4b72cb0b584710e64051d70f
KaigaMunene <kaigamunene@gmail.com>
2021-10-15 12:18:24 +0300

    coded something new

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
-
-hello = "Hello"
-myname = "Kaiga"
-
-hello_name = hello + myname
-print(hello_name)
+print('Kaiga is awesome')
+if 1 < 2:
+    print("One is less than two")
+if 3 > 2:
+ print ("Three is greater than two")
 
